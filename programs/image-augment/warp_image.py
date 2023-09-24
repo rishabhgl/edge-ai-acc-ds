940382ae5771a4ea5042c609d2f2bcb3089c9c10
rishabhgl <rishabhisjayden@gmail.com>
2023-09-24 11:39:09 +0530

    Added perspective warp

---
+++ programs/image-augment/warp_image.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+mainpath = os.path.dirname(os.getcwd())
+image_tilt_path = os.path.join(mainpath, "images", "image2.jpg")
+image_strt_path = os.path.join(mainpath, "images", "image1.jpg")
+
+image_tilt = cv2.imread(image_tilt_path)
+image_tilt_RGB = cv2.cvtColor(image_tilt, cv2.COLOR_BGR2RGB)
+
+image_strt = cv2.imread(image_strt_path)
+image_strt_RGB = cv2.cvtColor(image_strt, cv2.COLOR_BGR2RGB)
+
+plt.imshow(image_tilt_RGB)
+tilt_ref = np.float32(plt.ginput(4))
+print(tilt_ref)
+
+plt.imshow(image_strt_RGB)
+strt_ref = np.float32(plt.ginput(4))
+print(strt_ref)
+
+transform = cv2.getPerspectiveTransform(tilt_ref, strt_ref)
+straightened = cv2.warpPerspective(image_tilt, transform, (image_strt.shape[1], image_strt.shape[0]))
+
+plt.imshow(image_strt_RGB)
+plt.title("Strightened image!")
+plt.show()
