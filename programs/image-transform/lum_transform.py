aa790f7056a2cdfea2329d3965669f229b0fb9bd
rishabhgl <rishabhisjayden@gmail.com>
2023-09-25 08:27:13 +0530

    illumination transform

--- programs/image-transform/lum_transform.py
+++ programs/image-transform/lum_transform.py
@@ -8,11 +8,14 @@ def transform_add(image, add_by):
 def transform_mult(image, factor):
     return np.clip(image * factor, 0, 255)
 
-def transform_log(image):
-    pass
+def transform_log(image, c):
+    lookup = np.array([np.log(i/255 + 1) * c * 255 for i in np.arange(0, 256)]).astype("uint8")
+    return cv2.LUT(image, lookup)
 
 def transform_gamma(image, gamma):
-    pass
+    gamma_inv = 1 / gamma
+    lookup = np.array([((i / 255) ** gamma_inv) * 255 for i in np.arange(0, 256)]).astype("uint8")
+    return cv2.LUT(image, lookup)
 
 mainpath = os.path.dirname(os.getcwd())
 imagepath = os.path.join(mainpath, "images", "hello.jpg")
@@ -23,14 +26,24 @@ image = cv2.resize(image, (image.shape[1] // 3, image.shape[0] // 3))
 cv2.imshow("Original image", image)
 cv2.waitKey(0)
 
-image_add = transform_add(image, 50)
+image_add = transform_add(image, 15)
 print(image_add.dtype)
 cv2.imshow("Transformed image", image_add)
 cv2.waitKey(0)
 
-image_mult = transform_mult(image, 10)
+image_mult = transform_mult(image, 3)
 print(image_mult.dtype)
 cv2.imshow("Transformed image", image_mult)
+cv2.waitKey(0)
+
+image_gamma = transform_gamma(image, 0.85)
+print(image_gamma.dtype)
+cv2.imshow("Transformed image", image_gamma)
+cv2.waitKey(0)
+
+image_log = transform_log(image, 1.2)
+print(image_log.dtype)
+cv2.imshow("Transformed image", image_log)
 cv2.waitKey(0)
 
 cv2.destroyAllWindows()
